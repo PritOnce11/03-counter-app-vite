c7eef6a1fe677c3d87fb12323b31e53858a03585
PritOnce11 <pritonce@gmail.com>
2023-10-26 15:19:01 +0200

    first commit

---
+++ src/FirstApp.jsx
@@ -0,0 +1,16 @@
+
+const newMessage = {
+    message: 'Hola Mundo',
+    title: 'Fernando'
+};
+
+export const FirstApp = () => {
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            {/* <code>{ JSON.stringify(newMessage) }</code> */}
+            <p>Soy un subtitulo</p>
+        </>
+    )
+}
